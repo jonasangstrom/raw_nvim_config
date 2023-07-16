abfaf2c9d548e2a4a66d5daa74a3c409f9a641b7
Jonas Ångström <jonas.angstrom@gradientech.se>
2023-07-16 22:00:24 +0200

    added neotree

--- init.lua
+++ init.lua
@@ -101,6 +101,16 @@ require('lazy').setup({
     },
   },
 
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+},
+
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
