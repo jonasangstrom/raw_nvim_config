b1aa0af54236d7c5117f23dadaee919a685fc3d7
Jonas Ångström <jonas.angstrom@gradientech.se>
2023-07-16 22:37:47 +0200

    better window navigation kemaps

--- init.lua
+++ init.lua
@@ -277,6 +277,14 @@ vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = tr
 
 vim.keymap.set('i', 'jk', "<Esc>", {})
 vim.keymap.set('n', '<leader>ft', ":Neotree<cr>", { desc = 'open [F]ile [Tree] in neotree' })
+vim.keymap.set('n', '<C-h>', '<C-w>h', {desc = 'Move to left split' })
+vim.keymap.set('n', '<C-j>', '<C-w>j', {desc = 'Move to below split' })
+vim.keymap.set('n', '<C-k>', '<C-w>k', {desc = 'Move to above split' })
+vim.keymap.set('n', '<C-l>', '<C-w>l', {desc = 'Move to right split' })
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', {desc = 'Resize split up' })
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', {desc = 'Resize split down' })
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', {desc = 'Resize split left' })
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', {desc = 'Resize split right' })
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
