433987c0e174b9d0bde4462260999b23c3323ec7
Jonas Ångström <jonas.aangstroem@gmail.com>
2023-12-28 00:34:06 +0100

    set background to transparent

--- init.lua
+++ init.lua
@@ -622,5 +622,9 @@ yarepl.setup {
   },
 }
 
+-- set background to transparent
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
